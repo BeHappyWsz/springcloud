93f43b355c2083a32d4deeab08599ebf019306b6
wsz <wangshangzhuang@kingtree.com.cn>
2020-07-02 21:04:18 +0800

    MOD 1.guava 2.调整配置信息读取模式

--- config-client/src/main/java/com/springcloud/configclient/config/CustomConfig.java
+++ config-client/src/main/java/com/springcloud/configclient/config/CustomConfig.java
@@ -1,7 +1,12 @@
 package com.springcloud.configclient.config;
 
+import com.google.common.collect.Maps;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
 
 /**
  * @Description :
@@ -9,8 +14,8 @@ import org.springframework.stereotype.Component;
  * @Date: 2020-07-02 20:54
  */
 @Component
+@Data
 public class CustomConfig {
-
 
     @Value("${name}")
     String name;
@@ -20,4 +25,12 @@ public class CustomConfig {
 
     @Value("${age}")
     int age;
+
+    public Map getConfig() {
+        Map data = Maps.newHashMap();
+        data.put("name", name);
+        data.put("age", age);
+        data.put("author", author);
+        return data;
+    }
 }
--- config-client/src/main/java/com/springcloud/configclient/web/ClientWeb.java
+++ config-client/src/main/java/com/springcloud/configclient/web/ClientWeb.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 /**
  * @Description :
  * @Author : wsz
@@ -19,7 +21,7 @@ public class ClientWeb {
     CustomConfig customConfig;
 
     @GetMapping(value = "")
-    public CustomConfig getConfig() {
-        return customConfig;
+    public Map getConfig() {
+        return customConfig.getConfig();
     }
 }
